c27c109cf11950f0d39e450db832286d36eadd5b
Martin Kolev <m.kolevtkd@gmail.com>
2022-09-03 20:52:45 +0300

    Added more arrays exercises.

---
+++ Programming Fundamentals C#/Fundamentals Projects/SoftUni_Fundamentals/Exercise03.1/03.3.RecursiveFibonacci/RecursiveFibonacci.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _03._3.RecursiveFibonacci
+{
+    class RecursiveFibonacci
+    {
+        public static long[] memo;
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            memo = new long[num + 1];
+
+            Console.WriteLine(GetFib(num - 1));
+        }
+
+        static long GetFib(int num)
+        {
+            if (num <= 1)
+            {
+                return memo[num] =  1;
+            }
+
+            if (memo[num] != 0)
+            {
+                return memo[num];
+            }
+
+            memo[num] = GetFib(num - 1) + GetFib(num - 2);
+
+            return memo[num];
+
+        }
+    }
+}
---
+++ Programming Fundamentals C#/Fundamentals Projects/SoftUni_Fundamentals/Exercise03.1/03.4.FoldAndSum/FoldAndSum.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._4.FoldAndSum
+{
+    internal class FoldAndSum
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int k = nums.Length / 4;
+            int[] topRow = new int[nums.Length / 2];
+            int topFills = 0;
+
+            for (int half1 = topRow.Length - k - 1; half1 >= 0; half1--)
+            {
+                topRow[topFills] = nums[half1];
+                topFills++;
+            }
+
+            for (int half2 = nums.Length - 1; half2 >= nums.Length - k; half2--)
+            {
+                topRow[topFills] = nums[half2];
+                topFills++;
+            }
+
+            int[] bottomRow = new int[nums.Length / 2];
+            int bottomFills = 0;
+
+            for (int midPart = k; midPart < nums.Length - k; midPart++)
+            {
+                bottomRow[bottomFills] = nums[midPart];
+                bottomFills++;
+            }
+
+            for (int i = 0; i < topRow.Length; i++)
+            {
+                Console.Write($"{topRow[i] + bottomRow[i]} ");
+            }
+        }
+    }
+}
