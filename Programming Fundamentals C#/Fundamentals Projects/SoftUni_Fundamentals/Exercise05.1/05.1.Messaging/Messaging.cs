e0dbecf0367ce87772dee84a015257aa881ea737
Martin Kolev <m.kolevtkd@gmail.com>
2022-09-06 00:57:14 +0300

    C# Lists
    Added 2 new solved problems of lists type.

---
+++ Programming Fundamentals C#/Fundamentals Projects/SoftUni_Fundamentals/Exercise05.1/05.1.Messaging/Messaging.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _05._1.Messaging
+{
+    internal class Messaging
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputNumbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            string inputMessage = Console.ReadLine();
+            StringBuilder message = new StringBuilder();
+
+            for (int index = 0; index < inputNumbers.Count; index++)
+            {
+                int sum = 0;
+                string element = inputNumbers[index].ToString();
+
+                foreach (var digit in element)
+                {
+                    sum += (int)char.GetNumericValue(digit);
+                }
+
+                if (sum >= inputMessage.Length)
+                {
+                    int diff = sum - inputMessage.Length;
+                    sum = diff;
+                }
+
+                char letter = inputMessage[sum];
+                inputMessage = inputMessage.Remove(sum, 1);
+                message.Append(letter);
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Programming Fundamentals C#/Fundamentals Projects/SoftUni_Fundamentals/Exercise05.1/05.2.CarRace/CarRace.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._2.CarRace
+{
+    internal class CarRace
+    {
+        static void Main(string[] args)
+        {
+            List<int> track = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            double sumLeft = 0;
+
+            for (int left = 0; left < track.Count / 2; left++)
+            {
+                if (track[left] == 0)
+                {
+                    sumLeft *= 0.8;
+                }
+
+                sumLeft += track[left];
+            }
+
+            double sumRight = 0;
+
+            for (int right = track.Count - 1; right > track.Count / 2; right--)
+            {
+                if (track[right] == 0)
+                {
+                    sumRight *= 0.8;
+                }
+
+                sumRight += track[right];
+            }
+
+            int integer = 0;
+            bool leftIsInt = int.TryParse(sumLeft.ToString(), out integer);
+            bool rightIsInt = int.TryParse(sumRight.ToString(), out integer);
+
+            if (sumLeft < sumRight)
+            {
+                Console.WriteLine(leftIsInt 
+                    ? $"The winner is left with total time: {sumLeft}"
+                    : $"The winner is left with total time: {sumLeft:f1}");
+            }
+
+            else if (sumRight < sumLeft)
+            {
+                Console.WriteLine(rightIsInt 
+                    ? $"The winner is right with total time: {sumRight}"
+                    : $"The winner is right with total time: {sumRight:f1}");
+            }
+        }
+    }
+}
