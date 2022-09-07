85190d660a6681a3f47ade40b65867ea0b49e30b
Martin Kolev <m.kolevtkd@gmail.com>
2022-09-07 19:26:43 +0300

    Lists ME
    Added last 2 solutions to lists ME.

---
+++ Programming Fundamentals C#/Fundamentals Projects/SoftUni_Fundamentals/Exercise05.1/05.4.MixedUpLists/MixedUpLists.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._4.MixedUpLists
+{
+    internal class MixedUpLists
+    {
+        static void Main(string[] args)
+        {
+            List<int> line1 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            List<int> line2 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Reverse().ToList();
+            List<int> mixedNums = new List<int>();
+            int shorter = Math.Min(line1.Count, line2.Count);
+
+            for (int index = 0; index < shorter; index++)
+            {
+                mixedNums.Add(line1[index]);
+                mixedNums.Add(line2[index]);
+            }
+
+            mixedNums = mixedNums.OrderBy(x => x).ToList();
+            line1.RemoveRange(0, shorter);
+            line2.RemoveRange(0, shorter);
+
+            List<int> range = line2;
+
+            if (line1.Count > line2.Count)
+            {
+                range = line1;
+            }
+
+            range.Sort();
+
+            for (int index = 0; index < mixedNums.Count; index++)
+            {
+                if (mixedNums[index] > range[0] && mixedNums[index] < range[1])
+                {
+                    Console.Write($"{mixedNums[index]} ");
+                }
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals C#/Fundamentals Projects/SoftUni_Fundamentals/Exercise05.1/05.5.DrumSet/DrumSet.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._5.DrumSet
+{
+    internal class DrumSet
+    {
+        static void Main(string[] args)
+        {
+            double savings = double.Parse(Console.ReadLine());
+            List<int> drumSet = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            List<int> initialDrums = new List<int>();
+            initialDrums.AddRange(drumSet);
+
+            while (true)
+            {
+                string end = Console.ReadLine();
+
+                if (end == "Hit it again, Gabsy!")
+                {
+                    break;
+                }
+
+                int hitPower = int.Parse(end);
+
+                for (int index = 0; index < drumSet.Count; index++)
+                {
+                    drumSet[index] -= hitPower;
+
+                    if (drumSet[index] <= 0)
+                    {
+                        int price = initialDrums[index] * 3;
+
+                        if (savings - price >= 0)
+                        {
+                            savings -= price;
+                            drumSet[index] = initialDrums[index];
+                        }
+
+                        else
+                        {
+                            drumSet.RemoveAt(index);
+                            initialDrums.RemoveAt(index);
+                            index--;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', drumSet));
+            Console.WriteLine($"Gabsy has {savings:f2}lv.");
+        }
+    }
+}
