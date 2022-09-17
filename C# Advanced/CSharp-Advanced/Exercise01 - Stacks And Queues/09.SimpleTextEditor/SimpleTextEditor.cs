8fb1b130903b01234ed1a9e8909cc256d54aadd1
Martin Kolev <m.kolevtkd@gmail.com>
2022-09-18 00:53:55 +0300

    Stacks and Queues more solutions

---
+++ C# Advanced/CSharp-Advanced/Exercise01 - Stacks And Queues/09.SimpleTextEditor/SimpleTextEditor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09.SimpleTextEditor
+{
+    internal class SimpleTextEditor
+    {
+        static void Main(string[] args)
+        {
+            int operationsNum = int.Parse(Console.ReadLine());
+            StringBuilder text = new StringBuilder();
+            Stack<string> prevVersions = new Stack<string>();
+            string previousVersion = text.ToString();
+
+            for (int op = 0; op < operationsNum; op++)
+            {
+                string[] cmdArgs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string cmd = cmdArgs[0];
+
+                if (cmd == "1")
+                {
+                    string textToAppend = cmdArgs[1];
+
+                    previousVersion = text.ToString();
+                    prevVersions.Push(previousVersion);
+                    text.Append(textToAppend);
+                }
+
+                else if (cmd == "2")
+                {
+                    int count = int.Parse(cmdArgs[1]);
+                    int startIndex = text.Length - count;
+
+                    previousVersion = text.ToString();
+                    prevVersions.Push(previousVersion);
+                    text = text.Remove(startIndex, count);
+                }
+
+                else if (cmd == "3")
+                {
+                    int index = int.Parse(cmdArgs[1]);
+                    Console.WriteLine(text[index - 1]);
+                }
+
+                else if (cmd == "4")
+                {
+                    text = new StringBuilder(prevVersions.Pop());
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/CSharp-Advanced/Exercise01 - Stacks And Queues/10.Crossroads/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10.Crossroads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int greenDuration = int.Parse(Console.ReadLine());
+            int freeWindow = int.Parse(Console.ReadLine());
+            int greenLight = greenDuration + freeWindow;
+            Queue<string> cars = new Queue<string>();
+
+            while (true)
+            {
+                string car = Console.ReadLine();
+
+                if (car == "END")
+                {
+                    break;
+                }
+
+                else if (car != "green")
+                {
+                    cars.Enqueue(car);
+                    continue;
+                }
+
+                foreach (string vehicle in cars)
+                {
+                    string currentCar = vehicle;
+
+                    for (int secs = 0; secs < greenLight; secs++)
+                    {
+                        if (cu)
+                        {
+
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
