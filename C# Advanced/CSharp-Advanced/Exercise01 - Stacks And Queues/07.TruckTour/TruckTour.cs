f80f130d3a72a1ed43d351fd54cb0ad48cfe0910
Martin Kolev <m.kolevtkd@gmail.com>
2022-09-17 22:31:29 +0300

    Stacks and Queues update.
    Solved 2 more problems from the exercise of Stacks and Queues.

---
+++ C# Advanced/CSharp-Advanced/Exercise01 - Stacks And Queues/07.TruckTour/TruckTour.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.TruckTour
+{
+    internal class TruckTour
+    {
+        static void Main(string[] args)
+        {
+            int petrolPumps = int.Parse(Console.ReadLine());
+            Queue<long[]> pumps = new Queue<long[]>();
+
+            for (int i = 0; i < petrolPumps; i++)
+            {
+                long[] currentPump = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+                pumps.Enqueue(currentPump);
+            }
+
+            for (int index = 0; index < pumps.Count; index++)
+            {
+                long tankCapacity = 0;
+                long[] startingPump = pumps.Peek();
+                long fuel = startingPump[0];
+                long distance = startingPump[1];
+
+                if (fuel < distance)
+                {
+                    pumps.Dequeue();
+                    pumps.Enqueue(startingPump);
+                    continue;
+                }
+
+                pumps.Dequeue();
+                tankCapacity += fuel;
+                tankCapacity -= distance;
+                Queue<long[]> subQueue = new Queue<long[]>(pumps);
+
+                for (int pump = 0; pump < pumps.Count; pump++)
+                {
+                    long[] currentPump = pumps.Peek();
+                    long currentFuel = currentPump[0];
+                    long currentDistance = currentPump[1];
+
+                    if (currentPump == startingPump)
+                    {
+                        int bestRouteIndex = index;
+                        Console.WriteLine(bestRouteIndex);
+
+                        return;
+                    }
+
+                    pumps.Enqueue(startingPump);
+
+                    if (tankCapacity + currentFuel < currentDistance)
+                    {
+                        pumps = subQueue;
+                        pumps.Enqueue(startingPump);
+                        break;
+                    }
+
+                    tankCapacity += currentFuel;
+                    tankCapacity -= currentDistance;
+                    pumps.Dequeue();
+                    pumps.Enqueue(currentPump);
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/CSharp-Advanced/Exercise01 - Stacks And Queues/08.BalancedParentheses/BalancedParentheses.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.BalancedParentheses
+{
+    internal class BalancedParentheses
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+            Stack<char> parentheses = new Stack<char>();
+
+            foreach (char symbol in input)
+            {
+                if (parentheses.Any())
+                {
+                    if (parentheses.Peek() == '(' && symbol == ')')
+                    {
+                        parentheses.Pop();
+                        continue;
+                    }
+
+                    else if (parentheses.Peek() == '{' && symbol == '}')
+                    {
+                        parentheses.Pop();
+                        continue;
+                    }
+
+                    else if (parentheses.Peek() == '[' && symbol == ']')
+                    {
+                        parentheses.Pop();
+                        continue;
+                    }
+                }
+                parentheses.Push(symbol);
+            }
+
+            Console.WriteLine(!parentheses.Any() ? "YES" : "NO");
+        }
+    }
+}
