295dd82fe20ebc73fcf825f1669566f9e6bd5969
Martin Kolev <m.kolevtkd@gmail.com>
2022-09-23 23:57:47 +0300

    Halfway done Streams, Files and Directories Lab

---
+++ C# Advanced/CSharp-Advanced/Lab04 - Streams, Files and Directories/01.OddLines/OddLines.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+
+            using (reader)
+            {
+                int counter = 0;
+                string line = reader.ReadLine();
+                StreamWriter writer = new StreamWriter(outputFilePath);
+
+                using (writer)
+                {
+                    while (line != null)
+                    {
+                        if (counter % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/CSharp-Advanced/Lab04 - Streams, Files and Directories/03.WordCount/WordCount.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace WordCount
+{
+    public class WordCount
+    {
+        static void Main()
+        {
+            string wordPath = @"..\..\..\Files\words.txt";
+            string textPath = @"..\..\..\Files\text.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            StreamReader readerWords = new StreamReader(wordsFilePath);
+            string[] wordsLine = readerWords.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> wordUses = new Dictionary<string, int>();
+
+            using (readerWords)
+            {
+                foreach (string word in wordsLine)
+                {
+                    wordUses.Add(word, 0);
+                }
+
+                StreamReader readerText = new StreamReader(textFilePath);
+
+                using (readerText)
+                {
+                    string pattern = @"[A-Za-z]+";
+                    string textLine = readerText.ReadLine();
+
+                    while (textLine != null)
+                    {
+                        MatchCollection matches = Regex.Matches(textLine, pattern);
+
+                        foreach (var word in wordUses.ToDictionary(x => x.Key, y => y.Value))
+                        {
+                            foreach (Match match in matches)
+                            {
+                                if (match.ToString().ToLower() == word.Key.ToLower())
+                                {
+                                    wordUses[word.Key]++;
+                                }
+                            }
+                        }
+
+                        textLine = readerText.ReadLine();
+                    }
+
+                    wordUses = wordUses.OrderByDescending(y => y.Value).ToDictionary(x => x.Key, y => y.Value);
+                }
+
+                StreamWriter writer = new StreamWriter(outputFilePath);
+
+                using (writer)
+                {
+                    foreach (var kvp in wordUses)
+                    {
+                        writer.WriteLine($"{kvp.Key} - {kvp.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
+
+
