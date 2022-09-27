3f9c5392dabb96028f57f0b49939a5c956fd695e
Martin Kolev <m.kolevtkd@gmail.com>
2022-09-28 00:22:51 +0300

    Partially solved Functional Programming Lab.

---
+++ C# Advanced/CSharp-Advanced/Lab05 - Functional Programming/03.CountUppercaseWords/CountUppercaseWords.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _03.CountUppercaseWords
+{
+    internal class CountUppercaseWords
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> isCapitalLetter = x => char.IsUpper(x[0]);
+
+            Console.WriteLine(string.Join("\r\n",
+            Array.FindAll(Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries), isCapitalLetter)));
+        }
+    }
+}
---
+++ C# Advanced/CSharp-Advanced/Lab05 - Functional Programming/04.AddVAT/AddVAT.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.AddVAT
+{
+    internal class AddVAT
+    {
+        static void Main(string[] args)
+        {
+            Func<decimal, string> VAT = x => (x * 1.2M).ToString("f2");
+
+            Console.WriteLine(String.Join(Environment.NewLine,
+                Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse)
+                .Select(VAT)));
+        }
+    }
+}
