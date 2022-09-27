36c18638b34312984fc96cf1b77e830f8b9e7477
Martin Kolev <m.kolevtkd@gmail.com>
2022-09-27 23:55:22 +0300

    Partially solved Functional Programming Lab.

---
+++ C# Advanced/CSharp-Advanced/Lab05 - Functional Programming/02.SumNumbers/SumNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SumNumbers
+{
+    internal class SumNumbers
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> parser = n => int.Parse(n);
+
+            int[] numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(parser)
+                .ToArray();
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
---
+++ C# Advanced/CSharp-Advanced/Lab05 - Functional Programming/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _03.CountUppercaseWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> isCapitalLetter = x => char.IsUpper(x[0]);
+
+            
+                
+        }
+    }
+}
