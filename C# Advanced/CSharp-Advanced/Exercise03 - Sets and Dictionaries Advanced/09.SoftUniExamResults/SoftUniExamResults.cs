6e0e2815ed1237e7d17481224de790db71f6b4ef
Martin Kolev <m.kolevtkd@gmail.com>
2022-09-28 23:41:00 +0300

    Sets and Dictionaries Advanced done.

---
+++ C# Advanced/CSharp-Advanced/Exercise03 - Sets and Dictionaries Advanced/09.SoftUniExamResults/SoftUniExamResults.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.SoftUniExamResults
+{
+    internal class SoftUniExamResults
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> participants = new Dictionary<string, int>();
+            Dictionary<string, int> languages = new Dictionary<string, int>();
+
+            while (true)
+            {
+                string[] submissionInfo = Console.ReadLine().Split('-', StringSplitOptions.RemoveEmptyEntries);
+                string participantName = submissionInfo[0];
+
+                if (submissionInfo[0] == "exam finished")
+                {
+                    break;
+                }
+
+                if (submissionInfo[1] == "banned")
+                {
+                    participants.Remove(participantName);
+                    continue;
+                }
+
+                string languageName = submissionInfo[1];
+                int points = int.Parse(submissionInfo[2]);
+
+                if (!participants.ContainsKey(participantName))
+                {
+                    participants.Add(participantName, 0);
+                }
+
+                if (participants[participantName] < points)
+                {
+                    participants[participantName] = points;
+                }
+
+                if (!languages.ContainsKey(languageName))
+                {
+                    languages.Add(languageName, 0);
+                }
+
+                languages[languageName]++;
+            }
+
+            participants = participants
+                .OrderByDescending(x => x.Value)
+                .ThenBy(x => x.Key)
+                .ToDictionary(x => x.Key, y => y.Value);
+
+            Console.WriteLine("Results:");
+
+            foreach (KeyValuePair<string, int> participant in participants)
+            {
+                Console.WriteLine($"{participant.Key} | {participant.Value}");
+            }
+
+            languages = languages
+                .OrderByDescending(x => x.Value)
+                .ThenBy(x => x.Key)
+                .ToDictionary(x => x.Key, y => y.Value);
+
+            Console.WriteLine("Submissions:");
+
+            foreach (KeyValuePair<string, int> language in languages)
+            {
+                Console.WriteLine($"{language.Key} - {language.Value}");
+            }
+        }
+    }
+}
---
+++ C# Advanced/CSharp-Advanced/Exercise03 - Sets and Dictionaries Advanced/10.ForceBook/ForceBook.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10.ForceBook
+{
+    internal class ForceBook
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, SortedSet<string>> sides = new SortedDictionary<string, SortedSet<string>>();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Lumpawaroo")
+                {
+                    break;
+                }
+
+                if (command.Contains('|'))
+                {
+                    string[] cmdArgs = command.Split(" | ", StringSplitOptions.RemoveEmptyEntries);
+                    string forceSide = cmdArgs[0];
+                    string forceUser = cmdArgs[1];
+
+                    if (!sides.Values.Any(x => x.Contains(forceUser)))
+                    {
+                        if (!sides.ContainsKey(forceSide))
+                        {
+                            sides.Add(forceSide, new SortedSet<string>());
+                        }
+
+                        sides[forceSide].Add(forceUser);
+                    }
+                }
+
+                else if (command.Contains("->"))
+                {
+                    string[] cmdArgs = command.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                    string forceUser = cmdArgs[0];
+                    string forceSide = cmdArgs[1];
+
+                    foreach (var side in sides)
+                    {
+                        if (side.Value.Contains(forceUser))
+                        {
+                            side.Value.Remove(forceUser);
+                            break;
+                        }
+                    }
+
+                    if (!sides.ContainsKey(forceSide))
+                    {
+                        sides.Add(forceSide, new SortedSet<string>());
+                    }
+
+                    Console.WriteLine($"{forceUser} joins the {forceSide} side!");
+                    sides[forceSide].Add(forceUser);
+                }
+            }
+
+            var sortedSides = sides.OrderByDescending(x => x.Value.Count);
+
+            foreach (var side in sortedSides)
+            {
+                if (side.Value.Count > 0)
+                {
+                    Console.WriteLine($"Side: {side.Key}, Members: {side.Value.Count}");
+                }
+
+                foreach (var user in side.Value)
+                {
+                    Console.WriteLine($"! {user}");
+                }
+            }
+        }
+    }
+}
