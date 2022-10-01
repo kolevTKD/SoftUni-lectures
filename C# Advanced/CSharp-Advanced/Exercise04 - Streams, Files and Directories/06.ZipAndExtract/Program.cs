d6319f4ca9473c20a1548ba81be90ce83263500c
Martin Kolev <m.kolevtkd@gmail.com>
2022-10-01 23:46:57 +0300

    Streams, Files And Directories Ex. almost finished

---
+++ C# Advanced/CSharp-Advanced/Exercise04 - Streams, Files and Directories/06.ZipAndExtract/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace ZipAndExtract
+{
+    public class ZipAndExtract
+    {
+        static void Main()
+        {
+        }
+
+        public static void ZipFileToArchive(string inputFilePath, string zipArchiveFilePath)
+        {
+        }
+
+        public static void ExtractFileFromArchive(string zipArchiveFilePath, string fileName, string outputFilePath)
+        {
+        }
+    }
+}
+
