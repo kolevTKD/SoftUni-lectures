226a033559447632dcb68878bd48e4afd1bc74c0
Martin Kolev <m.kolevtkd@gmail.com>
2022-10-03 01:10:31 +0300

    Defining Classes lab 4/5 problems solved

---
+++ C# Advanced/CSharp-Advanced/Lab06 - Defining Classes/04.CarEngineAndTires/Engine.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    class Engine
+    {
+        private int horsePower;
+        private double cubicCapacity;
+
+        public Engine(int horsePower, double cubicCpacity)
+        {
+            this.HorsePower = horsePower;
+            this.CubicCapacity = cubicCpacity;
+        }
+
+        public int HorsePower
+        {
+            get
+            {
+                return this.horsePower;
+            }
+            set
+            {
+                this.horsePower = value;
+            }
+        }
+
+        public double CubicCapacity
+        {
+            get
+            {
+                return this.cubicCapacity;
+            }
+            set
+            {
+                this.cubicCapacity = value;
+            }
+        }
+    }
+}
+
