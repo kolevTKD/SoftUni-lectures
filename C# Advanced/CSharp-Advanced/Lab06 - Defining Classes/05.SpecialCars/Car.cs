0b2a3fcc16ec2a8e36feb229ea1a929addc771db
Martin Kolev <m.kolevtkd@gmail.com>
2022-10-04 19:06:04 +0300

    Defining classes lab done.

---
+++ C# Advanced/CSharp-Advanced/Lab06 - Defining Classes/05.SpecialCars/Car.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+        private Engine engine;
+        private Tire[] tires;
+
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year)
+            : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+            : this(make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires)
+            : this(make, model, year, fuelQuantity, fuelConsumption)
+        {
+            this.Engine = engine;
+            this.Tires = tires;
+        }
+
+        public string Make
+        {
+            get
+            {
+                return this.make;
+            }
+            set
+            {
+                this.make = value;
+            }
+        }
+
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            set
+            {
+                this.model = value;
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return this.year;
+            }
+            set
+            {
+                this.year = value;
+            }
+        }
+
+        public double FuelQuantity
+        {
+            get
+            {
+                return this.fuelQuantity;
+            }
+            set
+            {
+                this.fuelQuantity = value;
+            }
+        }
+
+        public double FuelConsumption
+        {
+            get
+            {
+                return this.fuelConsumption;
+            }
+            set
+            {
+                this.fuelConsumption = value;
+            }
+        }
+
+        public Engine Engine
+        {
+            get
+            {
+                return this.engine;
+            }
+            set
+            {
+                this.engine = value;
+            }
+        }
+
+        public Tire[] Tires
+        {
+            get
+            {
+                return this.tires;
+            }
+            set
+            {
+                this.tires = value;
+            }
+        }
+        public void Drive(double distance)
+        {
+            double fuelConsumed = FuelConsumption * distance / 100;
+
+            if (FuelQuantity < fuelConsumed)
+            {
+                Console.WriteLine("Not enough fuel to perform the trip!");
+            }
+
+            else
+            {
+                FuelQuantity -= fuelConsumed;
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return
+            $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nHorsePowers: {this.Engine.HorsePower}\nFuelQuantity: {this.FuelQuantity}";
+        }
+    }
+}
---
+++ C# Advanced/CSharp-Advanced/Lab06 - Defining Classes/05.SpecialCars/SpecialCarsStartUp.cs
@@ -0,0 +1,90 @@
+﻿using CarManufacturer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.SpecialCars
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Tire[]> tires = new List<Tire[]>();
+
+            string input = Console.ReadLine();
+
+            while (input != "No more tires")
+            {
+                double[] tiresInfo = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(n => double.Parse(n)).ToArray();
+
+                tires.Add(new Tire[4]
+                {
+                    new Tire((int)tiresInfo[0], tiresInfo[1]),
+                    new Tire((int)tiresInfo[2], tiresInfo[3]),
+                    new Tire((int)tiresInfo[4], tiresInfo[5]),
+                    new Tire((int)tiresInfo[6], tiresInfo[7])
+                });
+
+                input = Console.ReadLine();
+            }
+
+            List<Engine> engines = new List<Engine>();
+
+            input = Console.ReadLine();
+
+            while (input != "Engines done")
+            {
+                double[] engineInfo = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(n => double.Parse(n)).ToArray();
+
+                int horsePower = (int)engineInfo[0];
+                double cubicCapacity = engineInfo[1];
+
+                Engine engine = new Engine(horsePower, cubicCapacity);
+                engines.Add(engine);
+
+                input = Console.ReadLine();
+            }
+
+            List<Car> cars = new List<Car>();
+
+            input = Console.ReadLine();
+
+            while (input != "Show special")
+            {
+                string[] carInfo = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string make = carInfo[0];
+                string model = carInfo[1];
+                int year = int.Parse(carInfo[2]);
+                double fuelQuantity = double.Parse(carInfo[3]);
+                double fuelConsumption = double.Parse(carInfo[4]);
+                int engineIndex = int.Parse(carInfo[5]);
+                int tiresIndex = int.Parse(carInfo[6]);
+
+                Car car = new Car(make, model, year, fuelQuantity, fuelConsumption, engines[engineIndex], tires[tiresIndex]);
+                cars.Add(car);
+
+                input = Console.ReadLine();
+            }
+
+            foreach (Car car in cars)
+            {
+                if (car.Year >= 2017 && car.Engine.HorsePower > 330)
+                {
+                    double tiresPressure = 0;
+
+                    foreach (Tire tire in car.Tires)
+                    {
+                        tiresPressure += tire.Pressure;
+                    }
+
+                    if (tiresPressure >= 9 && tiresPressure <= 10)
+                    {
+                        car.Drive(20);
+                        Console.WriteLine(car.WhoAmI());
+                    }
+                }
+            }
+        }
+    }
+}
