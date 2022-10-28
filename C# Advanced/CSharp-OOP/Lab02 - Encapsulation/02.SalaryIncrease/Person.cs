5b436e5624502846cd3b5f2c402fc235280edecf
Martin Kolev <m.kolevtkd@gmail.com>
2022-10-28 22:56:24 +0300

    Encapsulation Lab done.

---
+++ C# Advanced/CSharp-OOP/Lab02 - Encapsulation/02.SalaryIncrease/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        public decimal Salary { get; private set; }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age < 30)
+            {
+                percentage /= 2;
+            }
+
+            Salary += Salary * percentage / 100;
+        }
+
+        public override string ToString()
+        {
+            return
+                $"{FirstName} {LastName} receives {Salary:f2} leva.";
+        }
+    }
+}
---
+++ C# Advanced/CSharp-OOP/Lab02 - Encapsulation/04.FirstAndReserveTeam/Team.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+
+        public string Name { get; set; }
+        public IReadOnlyCollection<Person> FirstTeam { get { return this.firstTeam.AsReadOnly(); } }
+        public IReadOnlyCollection<Person> ReserveTeam { get { return this.reserveTeam.AsReadOnly(); } }
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                this.firstTeam.Add(person);
+            }
+
+            else
+            {
+                this.reserveTeam.Add(person);
+            }
+        }
+
+        public override string ToString()
+        {
+            return
+                $"First team has {firstTeam.Count} players." + Environment.NewLine +
+                $"Reserve team has {reserveTeam.Count} players.";
+        }
+    }
+}
