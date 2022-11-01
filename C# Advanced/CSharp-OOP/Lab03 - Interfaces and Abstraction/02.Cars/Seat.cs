223fe1d7807a21e5852b090a61d40ea3b1f90bb2
Martin Kolev <m.kolevtkd@gmail.com>
2022-11-01 23:36:23 +0200

    Interfaces and Abstraction Lab done.

---
+++ C# Advanced/CSharp-OOP/Lab03 - Interfaces and Abstraction/02.Cars/Seat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public string Start() => "Engine start";
+        public string Stop() => "Breaaak!";
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Color} {GetType().Name} {Model}")
+                .AppendLine($"{Start()}")
+                .AppendLine($"{Stop()}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced/CSharp-OOP/Lab03 - Interfaces and Abstraction/02.Cars/Tesla.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : IElectricCar, ICar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Battery { get; set; }
+
+        public string Start() => "Engine start";
+        public string Stop() => "Breaaak!";
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Color} {GetType().Name} {Model} with {Battery} Batteries")
+                .AppendLine($"{Start()}")
+                .AppendLine($"{Stop()}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
