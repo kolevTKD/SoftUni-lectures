0c23cac2ac557c09206b5d66981f3c7dcb9644e3
Martin Kolev <m.kolevtkd@gmail.com>
2022-11-05 01:52:45 +0200

    Interfaces and Abstraction Ex. Initial commit.

---
+++ C# Advanced/CSharp-OOP/Exercise03 - Interfaces and Abstraction/01.DefineAnInterfaceIPerson/Citizen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        public Citizen(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; protected set; }
+        public int Age { get; protected set; }
+    }
+}
---
+++ C# Advanced/CSharp-OOP/Exercise03 - Interfaces and Abstraction/02.MultipleImplementation/Citizen.cs
@@ -0,0 +1,17 @@
+﻿namespace PersonInfo
+{
+    public class Citizen : IPerson, IBirthable, IIdentifiable
+    {
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.Birthdate = birthdate;
+        }
+        public string Name { get; protected set; }
+        public int Age { get; protected set; }
+        public string Id { get; protected set; }
+        public string Birthdate { get; protected set; }
+    }
+}
