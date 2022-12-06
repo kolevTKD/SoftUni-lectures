36bde96615857e7c659869212df5c45f3e396f4c
Martin Kolev <m.kolevtkd@gmail.com>
2022-12-07 01:01:09 +0200

    Exception and Error handling Lab done.

---
+++ C# Advanced/CSharp-OOP/Lab - Exception and Error Handling/05.PlayCatch/PlayCatch.cs
@@ -0,0 +1,75 @@
+﻿namespace PlayCatch
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class PlayCatch
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArr = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).ToArray();
+            int exceptionCounter = 0;
+            
+
+            while (exceptionCounter < 3)
+            {
+                try
+                {
+                    string[] cmdArgs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    string cmd = cmdArgs[0];
+                    int index = int.Parse(cmdArgs[1]);
+
+                    if (cmd == "Replace")
+                    {
+                        int element = int.Parse(cmdArgs[2]);
+
+                        Replace(index, element, numsArr);
+                    }
+
+                    else if (cmd == "Print")
+                    {
+                        int endIndex = int.Parse(cmdArgs[2]);
+
+                        Print(index, endIndex, numsArr);
+                    }
+
+                    else if (cmd == "Show")
+                    {
+                        Show(index, numsArr);
+                    }
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("The index does not exist!");
+                    exceptionCounter++;
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("The index does not exist!");
+                    exceptionCounter++;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("The variable is not in the correct format!");
+                    exceptionCounter++;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", numsArr));
+        }
+
+        public static void Replace(int index, int element, int[] nums) => nums[index] = element;
+
+        public static void Print(int startIndex, int endIndex, int[] nums)
+        {
+            string[] numsString = nums.Select(n => n.ToString()).ToArray();
+            
+            int count = endIndex - startIndex + 1;
+            Console.WriteLine(String.Join(", ", numsString, startIndex, count));
+        }
+
+        public static void Show(int index, int[] nums) => Console.WriteLine(nums[index]);
+
+    }
+}
---
+++ C# Advanced/CSharp-OOP/Lab - Exception and Error Handling/06.MoneyTransactions/MoneyTransactions.cs
@@ -0,0 +1,77 @@
+﻿namespace MoneyTransactions
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class MoneyTransactions
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, double> accBalance = new Dictionary<int, double>();
+            string[] accountsInfo = Console.ReadLine().Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+            FillAccountBook(accountsInfo, accBalance);
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] cmdArgs = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string cmd = cmdArgs[0];
+                int accNumber = int.Parse(cmdArgs[1]);
+                double sum = double.Parse(cmdArgs[2]);
+
+                try
+                {
+                    if (!accBalance.ContainsKey(accNumber))
+                    {
+                        throw new ArgumentException("Invalid account!");
+                    }
+
+                    if (cmd == "Deposit")
+                    {
+                        accBalance[accNumber] += sum;
+                    }
+
+                    else if (cmd == "Withdraw")
+                    {
+                        if (accBalance[accNumber] < sum)
+                        {
+                            throw new ArgumentException("Insufficient balance!");
+                        }
+
+                        accBalance[accNumber] -= sum;
+                    }
+
+                    else
+                    {
+                        throw new ArgumentException("Invalid command!");
+                    }
+
+                    Console.WriteLine($"Account {accNumber} has new balance: {accBalance[accNumber]:F2}");
+                    
+                }
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Enter another command");
+                }
+            }
+        }
+
+        public static void FillAccountBook(string[] accsInfo, Dictionary<int, double> accBalance)
+        {
+            for (int i = 0; i < accsInfo.Length; i++)
+            {
+                string[] currAcc = accsInfo[i].Split('-', StringSplitOptions.RemoveEmptyEntries);
+                int currAccNum = int.Parse(currAcc[0]);
+                double currAccSum = double.Parse(currAcc[1]);
+
+                accBalance.Add(currAccNum, currAccSum);
+            }
+        }
+    }
+}
