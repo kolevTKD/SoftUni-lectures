d7d7cd492f014fd8a83d30803fa606fc258a9ed8
Martin Kolev <m.kolevtkd@gmail.com>
2023-09-08 01:15:49 +0300

    Inheritance Exercise halfway done.

---
+++ C# Advanced/OOP C#/01 - Inheritance/01. CSharp-OOP-Inheritance-Exercise-Skeleton/Person/Child.cs
@@ -0,0 +1,29 @@
+﻿namespace Person
+{
+    using System;
+    public class Child : Person
+    {
+
+        public Child(string name, int age)
+            : base(name, age)
+        {
+
+        }
+
+        public override int Age
+        {
+            get
+            {
+                return base.Age;
+            }
+            set
+            {
+                if (value <= 15)
+                {
+                    base.Age = value;
+                }
+            }
+
+        }
+    }
+}
---
+++ C# Advanced/OOP C#/01 - Inheritance/01. CSharp-OOP-Inheritance-Exercise-Skeleton/Person/Person.cs
@@ -0,0 +1,44 @@
+﻿namespace Person
+{
+    using System;
+    using System.Text;
+
+    public class Person
+    {
+        private int age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        
+        public virtual int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    age = value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}",
+                                 this.Name,
+                                 this.Age));
+
+            return stringBuilder.ToString();
+
+        }
+    }
+}
