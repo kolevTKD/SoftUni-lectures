0559cae358219b8d1743e5dfb7cbc512b366ce6f
Martin Kolev <m.kolevtkd@gmail.com>
2023-09-24 01:55:03 +0300

    Reflection And Attributes Lab started.

---
+++ C# Advanced/CSharp-OOP-Redo/Lab06 - Reflection and Attributes/01.Stealer/Spy.cs
@@ -0,0 +1,33 @@
+﻿namespace Stealer
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+
+    public class Spy
+    {
+        private StringBuilder sb;
+
+        public Spy()
+        {
+            sb = new StringBuilder();
+        }
+        public string StealFieldInfo(string investigatedClassName, params string[] fieldsToInvestigates)
+        {
+            Type type = Type.GetType(investigatedClassName);
+            FieldInfo[] fieldsInfo = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            object objType = Activator.CreateInstance(type);
+
+            sb.AppendLine($"Class under investigation: {type.Name}");
+
+            foreach (FieldInfo field in fieldsInfo.Where(f => fieldsToInvestigates.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(objType)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ C# Advanced/CSharp-OOP-Redo/Lab06 - Reflection and Attributes/02.HighQualityMistakes/Spy.cs
@@ -0,0 +1,61 @@
+﻿namespace HighQualityMistakes
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+
+    public class Spy
+    {
+        private StringBuilder sb;
+
+        public Spy()
+        {
+            sb = new StringBuilder();
+        }
+        public string StealFieldInfo(string investigatedClassName, params string[] fieldsToInvestigates)
+        {
+            Type type = Type.GetType(investigatedClassName);
+            FieldInfo[] fieldsInfo = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            object objType = Activator.CreateInstance(type);
+
+            sb.AppendLine($"Class under investigation: {type.Name}");
+
+            foreach (FieldInfo field in fieldsInfo.Where(f => fieldsToInvestigates.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(objType)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public string AnalyzeAccessModifiers(string className)
+        {
+            Type type = Type.GetType($"HighQualityMistakes.{className}");
+            FieldInfo[] fieldsInfo = type.GetFields();
+            //object objType = Activator.CreateInstance(type);
+
+            foreach (FieldInfo field in fieldsInfo)
+            {
+                sb.AppendLine($"{field.Name} must be private!");
+            }
+
+            MethodInfo[] nonPublicGetters = type.GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic);
+
+            foreach (MethodInfo methodGet in nonPublicGetters.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{methodGet.Name} have to be public!");
+            }
+
+            MethodInfo[] publicSetters = type.GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public);
+
+            foreach (MethodInfo methodSet in publicSetters.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{methodSet.Name} have to be private!");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
