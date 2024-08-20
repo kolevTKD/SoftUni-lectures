476d3f0343131c804afa76672fc19aaf23bb5ad2
Martin Kolev <m.kolevtkd@gmail.com>
2024-08-21 01:59:35 +0300

    TableRelations first exercise structure created

---
+++ C# DB/CSharp-Entity Framework Core/04 - Entity Relations - Recap/1.StudentSystem/P01_StudentSystem/StartUp.cs
@@ -0,0 +1,10 @@
+﻿namespace P01_StudentSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
