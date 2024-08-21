1bf0cd1f609b06f2b6c323376671604830d7d8dd
Martin Kolev <m.kolevtkd@gmail.com>
2024-08-21 22:06:47 +0300

    TableRelations first exercise structure created

--- C# DB/CSharp-Entity Framework Core/04 - Entity Relations - Recap/1.StudentSystem/P01_StudentSystem.Data/Migrations/20240821190445_DBVersion_1.1.Designer.cs
+++ C# DB/CSharp-Entity Framework Core/04 - Entity Relations - Recap/1.StudentSystem/P01_StudentSystem.Data/Migrations/20240821190445_DBVersion_1.1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using P01_StudentSystem.Data;
 
@@ -11,9 +12,10 @@ using P01_StudentSystem.Data;
 namespace P01_StudentSystem.Data.Migrations
 {
     [DbContext(typeof(StudentSystemContext))]
-    partial class StudentSystemDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240821190445_DBVersion_1.1")]
+    partial class DBVersion_11
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
