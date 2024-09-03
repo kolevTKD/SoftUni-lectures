6909fdfbfc7e8142cd81f48da8f0642e15886cae
Martin Kolev <m.kolevtkd@gmail.com>
2024-09-04 01:35:21 +0300

    Advanced Querying Testing project

---
+++ C# DB/CSharp-Entity Framework Core/Tests and Experiments/AdvancedQuryingTesting/Data/SoftUniContext.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using AdvancedQuryingTesting.Models;
+
+namespace AdvancedQuryingTesting.Data
+{
+    public partial class SoftUniContext : DbContext
+    {
+        public SoftUniContext()
+        {
+        }
+
+        public SoftUniContext(DbContextOptions<SoftUniContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Address> Addresses { get; set; } = null!;
+        public virtual DbSet<Department> Departments { get; set; } = null!;
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+        public virtual DbSet<Project> Projects { get; set; } = null!;
+        public virtual DbSet<Town> Towns { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=localhost;Database=SoftUni;Trusted_Connection=True;Trust Server Certificate=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Address>(entity =>
+            {
+                entity.HasOne(d => d.Town)
+                    .WithMany(p => p.Addresses)
+                    .HasForeignKey(d => d.TownId)
+                    .HasConstraintName("FK_Addresses_Towns");
+            });
+
+            modelBuilder.Entity<Department>(entity =>
+            {
+                entity.HasOne(d => d.Manager)
+                    .WithMany(p => p.Departments)
+                    .HasForeignKey(d => d.ManagerId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Departments_Employees");
+            });
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasOne(d => d.Address)
+                    .WithMany(p => p.Employees)
+                    .HasForeignKey(d => d.AddressId)
+                    .HasConstraintName("FK_Employees_Addresses");
+
+                entity.HasOne(d => d.Department)
+                    .WithMany(p => p.Employees)
+                    .HasForeignKey(d => d.DepartmentId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Employees_Departments");
+
+                entity.HasOne(d => d.Manager)
+                    .WithMany(p => p.InverseManager)
+                    .HasForeignKey(d => d.ManagerId)
+                    .HasConstraintName("FK_Employees_Employees");
+
+                entity.HasMany(d => d.Projects)
+                    .WithMany(p => p.Employees)
+                    .UsingEntity<Dictionary<string, object>>(
+                        "EmployeesProject",
+                        l => l.HasOne<Project>().WithMany().HasForeignKey("ProjectId").OnDelete(DeleteBehavior.ClientSetNull).HasConstraintName("FK_EmployeesProjects_Projects"),
+                        r => r.HasOne<Employee>().WithMany().HasForeignKey("EmployeeId").OnDelete(DeleteBehavior.ClientSetNull).HasConstraintName("FK_EmployeesProjects_Employees"),
+                        j =>
+                        {
+                            j.HasKey("EmployeeId", "ProjectId");
+
+                            j.ToTable("EmployeesProjects");
+
+                            j.IndexerProperty<int>("EmployeeId").HasColumnName("EmployeeID");
+
+                            j.IndexerProperty<int>("ProjectId").HasColumnName("ProjectID");
+                        });
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ C# DB/CSharp-Entity Framework Core/Tests and Experiments/AdvancedQuryingTesting/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace AdvancedQuryingTesting
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using Data;
+    using System.Text;
+
+    public class StartUp
+    {
+        static async Task Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            //var employees = await context.Employees.FromSqlRaw("SELECT * FROM Employees WHERE DepartmentId = 7").ToListAsync();
+
+            //int departmentId = int.Parse(Console.ReadLine());
+            //var employeesIParameterized = await context.Employees.FromSqlRaw("SELECT * FROM Employees WHERE DepartmentId = {0}", departmentId).ToListAsync();
+
+            string departmentName = Console.ReadLine();
+
+            //Working SQL Injection
+            //var employeesInterpolated = await context.Employees.FromSqlRaw($"SELECT e.* FROM Employees e JOIN Departments d ON e.DepartmentId = d.DepartmentId WHERE d.Name = '{departmentName}'").ToListAsync();
+
+            //Not working SQL Injection
+            //var employeesSqlRaw = await context.Employees.FromSqlRaw("SELECT e.* FROM Employees e JOIN Departments d ON e.DepartmentId = d.DepartmentId WHERE d.Name = '{0}'", departmentName).ToListAsync();
+
+            //Not working SQL Injection
+            var employeesInterpolated = await context.Employees.FromSqlInterpolated($"SELECT e.* FROM Employees e JOIN Departments d ON e.DepartmentId = d.DepartmentId WHERE d.Name = {departmentName}").ToListAsync();
+
+            StringBuilder sb = new StringBuilder();
+
+            int rowNumber = 1;
+            foreach (var employee in employeesInterpolated)
+            {
+                sb.AppendLine($"{rowNumber++}.{employee.FirstName} {employee.MiddleName} {employee.LastName} {employee.Salary:F2}");
+            }
+
+            Console.WriteLine(sb.ToString().TrimEnd());
+        }
+    }
+}
