d3f81ea42129649d6cb93582954adde0ced17aa3
Martin Kolev <m.kolevtkd@gmail.com>
2024-09-06 00:19:23 +0300

    AutoMapping Exercie project added

---
+++ C# DB/CSharp-Entity Framework Core/07 - Auto Mapping Objects/FastFood.Core/Controllers/CategoriesController.cs
@@ -0,0 +1,36 @@
+﻿namespace FastFood.Core.Controllers
+{
+    using System;
+    using AutoMapper;
+    using Data;
+    using Microsoft.AspNetCore.Mvc;
+    using ViewModels.Categories;
+
+    public class CategoriesController : Controller
+    {
+        private readonly FastFoodContext _context;
+        private readonly IMapper _mapper;
+
+        public CategoriesController(FastFoodContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateCategoryInputModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult All()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
