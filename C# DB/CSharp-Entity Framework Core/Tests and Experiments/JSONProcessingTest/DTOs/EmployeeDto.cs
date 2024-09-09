861f25be655c112e773e50996685f2b0303a176d
Martin Kolev <m.kolevtkd@gmail.com>
2024-09-10 01:09:45 +0300

    JSON Processing testing project created.

---
+++ C# DB/CSharp-Entity Framework Core/Tests and Experiments/JSONProcessingTest/DTOs/EmployeeDto.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace JSONProcessingTest.DTOs
+{
+    public class EmployeeDto 
+    {
+        public string FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string LastName { get; set; }
+
+        [JsonProperty("име")]
+        public string FullName { get; set; }
+
+        //When changing the name of the property, that's relevant to the column's name, the AutoMapper is able to map it to the according object (DepartmentName)
+        [JsonProperty("отдел")]
+        public string DepartmentName { get; set; }
+        [JsonProperty("заплата")]
+        public decimal Salary { get; set; }
+    }
+}
---
+++ C# DB/CSharp-Entity Framework Core/Tests and Experiments/JSONProcessingTest/Program.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using JSONProcessingTest.Data;
+using JSONProcessingTest.DTOs;
+using Newtonsoft.Json;
+using System.Text.Json;
+
+namespace JSONProcessingTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SoftUniContext context = new SoftUniContext())
+            {
+                var config = new MapperConfiguration(cfg =>
+                {
+                    cfg.AddProfile<MapProfile>();
+                });
+
+                var employees = context.Employees
+                    .ProjectTo<EmployeeDto>(config)
+                    .ToArray();
+
+                string path = @"..\..\..\employees.txt";
+
+                //System.Text.Json
+                //string serializedEmployees = JsonSerializer.Serialize(employees);
+
+                //File.WriteAllText(path, serializedEmployees);
+
+                //List<EmployeeDto> DTOs = JsonSerializer.Deserialize<List<EmployeeDto>>(serializedEmployees);
+
+                //int rowNumber = 1;
+                //foreach (EmployeeDto employee in DTOs)
+                //{
+                //    Console.WriteLine($"{rowNumber++}: {employee.FullName}");
+                //}
+
+                //Newtonsoft.Json
+                string serializedEmployees = JsonConvert.SerializeObject(employees, Formatting.Indented);
+                Console.WriteLine(serializedEmployees);
+
+                File.WriteAllText(path, serializedEmployees);
+
+                List<EmployeeDto> DTOs = JsonConvert.DeserializeObject<List<EmployeeDto>>(serializedEmployees);
+
+                int rowNumber = 1;
+
+                //foreach (EmployeeDto employee in DTOs)
+                //{
+                //    Console.WriteLine($"{rowNumber++}: {employee.FullName} - {employee.DepartmentName}");
+                //}
+            }
+        }
+    }
+}
